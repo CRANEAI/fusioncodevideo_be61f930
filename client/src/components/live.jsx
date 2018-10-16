8ce423e531d9e0671d36d6966f48414e754fc682
Elly <elly@mg.crane.ai>
2018-10-16 12:58:49 -0400

    updated client/src/components/live updated project details in new api

--- client/src/components/live.jsx
+++ client/src/components/live.jsx
@@ -7,32 +7,26 @@ created by: Anni on FusionCode
 // Component Requirements
 //----------------------------------------------------------------------------------------------------------
 
-the search view has a status bar located at the very-top across the full width of the view
-search view has Navigation Bar located at the very-top in the center of the view
-the search view has a Input Box with text e located at the very-top in the center of the view
-the search will appear on focus with state is set
-the live view has a back icon located at the very-top left of the view which on click will trigger the back() action
-the live view has a search icon located at the very-top left of the view which on click will trigger the search() action
-the live view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
-the signup view has a back icon located at the very-top left of the view which on click will trigger the back() action
-the signup view has a search icon located at the very-top left of the view which on click will trigger the search() action
-the signup view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
-the signup view has a back icon located at the very-top left of the view which on click will trigger the back() action
-the signup view has a search icon located at the very-top left of the view which on click will trigger the search() action
-the signup view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
-the composite view has a back icon located at the very-top left of the view which on click will trigger the back() action
-the composite view has a search icon located at the very-top left of the view which on click will trigger the search() action
-the composite view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
-the search view has a back icon located at the very-top left of the view which on click will trigger the back() action
-the search view has a search icon located at the very-top left of the view which on click will trigger the search() action
-the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
+the edit profile view has a status bar located at the very-top across the full width of the view
+edit profile view has Label located at the top center of the view, text needs identified
+edit profile view has Label located at the top center of the view, text needs identified
+edit profile view has Label located at the mid-top center of the view, text needs identified
+edit profile view has Label located at the mid-top center of the view, text needs identified
+edit profile view has Label located at the mid-top left of the view, text needs identified
+edit profile view has Label located at the mid-top left of the view, text needs identified
+edit profile view has Label located at the mid-top center of the view, text needs identified
+edit profile view has Label located at the mid-top center of the view, text needs identified
+the live view has a home icon located at the bottom left of the view which on click will trigger the home() action
+the live view has a profile icon located at the bottom right of the view which on click will trigger the profile() action
+the signup view has a home icon located at the bottom left of the view which on click will trigger the home() action
+the signup view has a profile icon located at the bottom right of the view which on click will trigger the profile() action
 
 */
         
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Row, Col , Navbar, NavItem, NavbarBrand } from 'reactstrap';
+import { Row, Col , Button } from 'reactstrap';
         
         
         import {  }  from '../actions/liveActions.js'; 
@@ -67,9 +61,26 @@ import { Row, Col , Navbar, NavItem, NavbarBrand } from 'reactstrap';
                  
                     <Col md={12}>
 
-                        <Navbar color="light" light expand="md"> 
-    <NavbarBrand><!-- todo: add text for navigation title --></NavbarBrand> 
-</Navbar> 
+                        				{/*<!-- TODO: Confirm label text --> */}
+
+			</Col>
+
+			<Col md={12}>
+				{/* <!-- TODO: Create unit test for /_edit_profile_\ action --> */}
+				<Button 
+                        className="btn btn-lg btn-block " 
+                        onClick={() => this./_edit_profile_\()}>/ edit profile \</Button> 
+
+			</Col>
+
+			<Col md={12}>
+				{/* <!-- TODO: Create unit test for r_‘ action --> */}
+				<Button 
+                        className="btn btn-lg btn-block " 
+                        onClick={() => this.r_‘()}>r ‘</Button> 
+
+			</Col>
+
 
                            
    
