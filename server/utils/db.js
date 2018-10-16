d154cf458e254a8e79249951761c02b05fdeafd1
Ryan Hickman <kingsrborn@gmail.com>
2018-10-16 12:20:52 -0400

    added the db config

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,2 +1,2 @@
 /* Removal static URL when deploying. Use ONLY env vars */
-module.exports = require("mongo-utility")(process.env.MONGO_URL || "mongodb://")
+module.exports = require("mongo-utility")(process.env.MONGO_URL || "mongodb://testuser:pl@yt1m3@ds043987.mlab.com:43987/fusioncodevideodemo")
