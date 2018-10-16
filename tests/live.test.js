11f570b28ce698c87e909f50c320cbd763b368f9
Elly <elly@mg.crane.ai>
2018-10-16 11:15:03 -0400

    added tests/live updated project details in new api

---
+++ tests/live.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - live UNIT TESTS
+ * Application Client
+ * fusioncodevideo
+*/
+
+/* signup view has Label located at the bottom center of the view, text needs identified */
+it("signup view has Label located at the bottom center of the view, text needs identified", () => {
+  /* ToDo developer needs to complete unit test 
+               signup view has Label located at the bottom center of the view, text needs identified
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
