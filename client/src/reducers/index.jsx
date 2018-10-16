dbf18e2807294d6bf9e6d90abed990579062524a
Elly <elly@mg.crane.ai>
2018-10-16 11:23:53 -0400

    updated client/src/reducers/index updated project details in new api

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -1,18 +1,16 @@
+import { combineReducers } from "redux"
+import { reducer as Form } from "redux-form"
 
-        import { combineReducers }                      from 'redux';
-        import { reducer as Form }                      from 'redux-form';
+import signup from "./reducer_signup"
+import live from "./reducer_live"
+import composite from "./reducer_composite"
+import sign_in from "./reducer_sign_in"
 
-        import \iup from './reducer_\iup' 
-import signup from './reducer_signup' 
-import live from './reducer_live'
+const rootReducer = combineReducers({
+  signup: signup,
+  live: live,
+  composite: composite,
+  sign_in: sign_in
+})
 
-        const rootReducer = combineReducers({
-            \iup : \iup 
-,signup : signup 
-,live : live 
-
-        })
-
-        export default rootReducer;
-
-        
+export default rootReducer
