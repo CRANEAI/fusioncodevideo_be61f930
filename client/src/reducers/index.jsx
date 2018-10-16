c48c82309d3f991ed20a7308244dbfae46d19a99
Elly <elly@mg.crane.ai>
2018-10-16 12:18:03 -0400

    updated client/src/reducers/index updated project details in new api

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -2,15 +2,21 @@ import { combineReducers } from "redux"
 import { reducer as Form } from "redux-form"
 
 import search from "./reducer_search"
-import back_action from "./reducer_back_action"
 import signup from "./reducer_signup"
+import menu from "./reducer_menu"
+import history from "./reducer_history"
+import edit_profile from "./reducer_edit_profile"
+import back_action from "./reducer_back_action"
 import signup from "./reducer_signup"
 import live from "./reducer_live"
 
 const rootReducer = combineReducers({
   search: search,
-  back_action: back_action,
   signup: signup,
+  menu: menu,
+  history: history,
+  edit_profile: edit_profile,
+  back_action: back_action,
   signup: signup,
   live: live
 })
