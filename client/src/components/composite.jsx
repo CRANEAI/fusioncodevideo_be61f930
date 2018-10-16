a1d4ba9272e4f90c944f78c328691eede9bc85d5
Elly <elly@mg.crane.ai>
2018-10-16 11:06:33 -0400

    added client/src/components/composite updated project details in new api

---
+++ client/src/components/composite.jsx
@@ -0,0 +1,74 @@
+
+/*
+composite view
+created by: Anni on FusionCode
+
+//----------------------------------------------------------------------------------------------------------
+// Component Requirements
+//----------------------------------------------------------------------------------------------------------
+
+
+
+*/
+        
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col   } from 'reactstrap';
+        
+        
+        import {  }  from '../actions/compositeActions.js'; 
+
+        
+        
+       
+        class compositeView extends React.Component {
+
+            state = {
+                
+                
+            }
+    
+                
+            
+
+            componentDidMount = () => {
+                
+               
+                
+            }
+    
+        render() {
+        
+        
+        const { data } = this.props
+
+            return (
+                <Row>
+
+                 
+                    <Col md={12}>
+
+                        
+                           
+   
+                    </Col> 
+                     
+                </Row>
+            )   
+        }
+    }
+                   
+    function mapStateToProps(state) {
+            
+            }
+        }
+    
+        function mapDispatchToProps(dispatch) {
+            return bindActionCreators({   }, dispatch);
+        }
+    
+   
+        export default connect(mapStateToProps, mapDispatchToProps)(compositeView);
+    
+        
