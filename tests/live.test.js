2268cd691e5c59a588e5904e11dab60b51c1d2c8
Elly <elly@mg.crane.ai>
2018-10-16 11:40:55 -0400

    updated tests/live updated project details in new api

--- tests/live.test.js
+++ tests/live.test.js
@@ -6,3 +6,16 @@
  * Application Client
  * fusioncodevideo
 */
+
+/* the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action */
+it("the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
