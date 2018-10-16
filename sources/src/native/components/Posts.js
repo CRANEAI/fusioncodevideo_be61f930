b8da3fb233bab857872fd79918e395781a972d54
Elly <elly@mg.crane.ai>
2018-10-16 14:15:15 -0400

    added sources/src/native/components/Posts updated project details in new api

---
+++ sources/src/native/components/Posts.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  FlatList, TouchableOpacity, RefreshControl, Image,
+} from 'react-native';
+import {
+  Container, Content, Card, CardItem, Body, Text, Button,
+} from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import Loading from './Loading';
+import Error from './Error';
+import Header from './Header';
+import Spacer from './Spacer';
+
+const RecipeListing = ({
+  error,
+  loading,
+  posts,
+  reFetch,
+}) => {
+  // Loading
+  if (loading) return <Loading />;
+
+  // Error
+  if (error) return <Error content={error} />;
+
+  const keyExtractor = item => item.id;
+
+  const onPress = item => Actions.post({ match: { params: { id: String(item.id) } } });
+
+  return (
+    <Container>
+      <Content >
+        <FlatList
+          numColumns={2}
+          data={posts}
+          renderItem={({ item }) => (
+            <Card transparent style={{ paddingHorizontal: 0 }}>
+              <CardItem cardBody>
+                <TouchableOpacity onPress={() => onPress(item)} style={{ flex: 1 }}>
+                  <Image
+                    source={{ uri: item.image }}
+                    style={{
+                      height: 185,
+                      width: null,
+                      flex: 1,
+                      borderRadius: 0,
+                    }}
+                  />
+                </TouchableOpacity>
+              </CardItem>
+              <CardItem cardBody>
+                <Body>
+                  <Spacer size={10} />
+                  <Text style={{ fontWeight: '800' }}>
+                    {item.title}
+                  </Text>
+                </Body>
+              </CardItem>
+            </Card>
+          )}
+          keyExtractor={keyExtractor}
+          refreshControl={(
+            <RefreshControl
+              refreshing={loading}
+              onRefresh={reFetch}
+            />
+          )}
+        />
+
+        <Spacer size={20} />
+      </Content>
+    </Container>
+  );
+};
+
+RecipeListing.propTypes = {
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  reFetch: PropTypes.func,
+};
+
+RecipeListing.defaultProps = {
+  error: null,
+  reFetch: null,
+};
+
+export default RecipeListing;
