d8d70e2c8455d6d82d05cec08ff7eb844ce77ed5
Elly <elly@mg.crane.ai>
2018-10-16 11:23:52 -0400

    added tests/sign_in updated project details in new api

---
+++ tests/sign_in.test.js
@@ -0,0 +1,25 @@
+
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - sign in UNIT TESTS
+ * Application Client
+ * fusioncodevideo
+*/
+
+
+
+            /* reset password view has a Label located at the bottom center of the view with text don't have an account? */
+            it('reset password view has a Label located at the bottom center of the view with text don't have an account?', () => {
+               /* ToDo developer needs to complete unit test 
+               reset password view has a Label located at the bottom center of the view with text don't have an account?
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+            });
+            
