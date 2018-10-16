8c3001372959d457fb453d3196642007927918c1
Elly <elly@mg.crane.ai>
2018-10-16 11:24:55 -0400

    updated server/api/v1/routes/signup updated project details in new api

--- server/api/v1/routes/signup.js
+++ server/api/v1/routes/signup.js
@@ -8,6 +8,30 @@ const { db } = require("../../../utils")
 
 /**
  * @swagger
+ * api/v1/signup/sign_in:
+ *   post:
+ *     tags:
+ *       - name: sign_in
+ *     description: sign_in
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: it works!
+ */
+router.post(
+  "/sign_in",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    sign_in().then(result => {
+      res.status(200).send(result)
+    })
+  }
+)
+
+/**
+ * @swagger
  * api/v1/signup/sign_up:
  *   post:
  *     tags:
@@ -29,6 +53,22 @@ router.post(
     })
   }
 )
+
+/**
+ * @swagger
+ * sign_in():
+ *
+ */
+sign_in = () => {
+  return new Promise((resolve, reject) => {
+    /* TODO - write sign_in function */
+    resolve()
+
+    /* TODO - handle sign_in error
+                        reject({message: "error"})
+                    */
+  })
+}
 
 /**
  * @swagger
